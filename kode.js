b95d64bc7c996fccd48cf08a8157466dee243d88
lolmaosteffen <steffen-1997@live.no>
2024-09-02 14:50:56 +0200

    wohoo

--- kode.js
+++ kode.js
@@ -11,28 +11,33 @@ function randomTall2() {
     return tall2;
 }
 function submitKnapp() {
-    let tall1 = randomTall1;
-    let tall2 = randomTall2;
-    let userInput = document.getElementById("inputfield").value.trim();
-    let currentScore = document.getElementById("poengtavla").innerHTML;
+    let tall1 = randomTall1();
+    let tall2 = randomTall2();
+    let userInput = document.getElementById("inputfield").value;
+    let currentScore = document.getElementById("poengtavla");
     let riktigSvar;
-    if (tall1 > tall2)
+    if (tall1 > tall2) {
         riktigSvar = '>';
-    else if (tall1 < tall2)
+    }
+    else if (tall1 < tall2) {
         riktigSvar = '<';
-    else (tall1 === tall2)
-    riktigSvar = '=';
+    }
+    else riktigSvar = '=';
 
-    if (userInput === riktigSvar)
-        document.getElementById("poengtavla").innerHTML = currentScore + 1;
-    else
-        document.getElementById("poengtavla").innerHTML = currentScore - 1;
+    if (userInput === riktigSvar) {
+        document.getElementById("poengtavla").innerHTML = +currentScore.innerHTML + 1;
+    }
+    else document.getElementById("poengtavla").innerHTML = +currentScore.innerHTML - 1;
     // document.getElementById("inputfield").value = '';
+    // if (userInput === '') {
+    //     document.getElementById("poengtavla").innerHTML = +currentScore.innerHTML + 1;
+    //     document.getElementById("poengtavla").innerHTML = currentScore.innerHTML - 1;
+    // }
 
-
+    console.log(riktigSvar, tall1, tall2);
 }
 function resetKnapp() {
-    randomTall1()
-    randomTall2()
+    // randomTall1()
+    // randomTall2()
     document.getElementById("inputfield").value = '';
 }
